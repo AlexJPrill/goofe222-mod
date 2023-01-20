d77c89b9d6143f436f1b8f880709238ed26d5147
AlexJPrill <alex.j.prill@gmail.com>
2023-01-20 11:06:13 -0600

    Added artillery-radar entity and recipe

--- data.lua
+++ data.lua
@@ -1,12 +1,16 @@
 --data.lua
 require("__goofe222-mod__/lib/core")
 
---Super uranium
+--Super-uranium
 require(items .. "super-uranium")
 require(recipes.."super-uranium-recipe")
 --require(items .."supre-uranium-fuel")
 
-
+--liquid-uranium
 require(fluids .. "liquid-uranium")
 require(recipes .."liquid-uranium-recipe")
+
+--artillery-radar
+require(entity.."artillery-radar")
+require(recipes.."artillery-radar-recipe")
 
---
+++ prototype/recipes/artillery-radar-recipe.lua
@@ -0,0 +1,22 @@
+data:extend(
+    {
+        {
+            type="recipe",
+            name="artillery-radar-recipe",
+            --I believe that this category is correct
+            category="machine",
+            energy_required=20,
+            ingredients=
+            {
+                {"iron_plate", 10},
+                {"electronic-circuit", 5},
+                {"iron-gear", 5},
+                {"radar", 1}
+            },
+            results=
+            {
+                {"artillery-radar", 1}
+            }
+        }
+    }
+)
