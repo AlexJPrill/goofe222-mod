e2568cba552ff6f440b82a7b966bb8a8d06d5783
AlexJPrill <alex.j.prill@gmail.com>
2023-01-22 12:39:53 -0600

    Changed it so would have base texture of uranium-235

--- prototype/items/super-uranium.lua
+++ prototype/items/super-uranium.lua
@@ -1,12 +1,18 @@
-data:extend(
+local super_uranium = table.deepcopy(data.raw["item"]["uranium-235"])
+--note seems that it is impossible to tint the uranium to make it a different color
+super_uranium.name="super-uranium"
+super_uranium.icons=
+{
     {
-        {
-            type="item",
-            name="super-uranium",
-            icon=graphics .."pk.png",
-            icon_size=64,
-            stack_size=100
-        }
-        
-    }
-)
+        icon=super_uranium.icon,
+        icon_size=64,
+        tine={r=255,g=1,b=1,a=1},
+        shift={4,-8}
+
+    },
+}
+
+
+
+data:extend{super_uranium}
+
